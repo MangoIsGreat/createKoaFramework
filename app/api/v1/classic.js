cbc7331891f9e69d009a2f285265b39006fc9acf
zp <859247268@qq.com>
2020-09-24 04:13:52 +0800

    使用LinValidator校验器

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,23 +1,15 @@
 const Router = require("koa-router")
 const router = new Router()
 
+const { PositiveIntegerValidator } = require("../../validators/validator")
+
 router.post("/v1/:id/classic/latest", (ctx, next) => {
     const path = ctx.params
     const query = ctx.request.query
     const headers = ctx.request.header
     const body = ctx.request.body
 
-    aaa
-
-    if (true) {
-        const error = new global.errs.ParameterException()
-
-        throw error
-    }
-
-    ctx.body = {
-        key: a
-    }
+    const v = new PositiveIntegerValidator().validate(ctx)
 })
 
 module.exports = router
