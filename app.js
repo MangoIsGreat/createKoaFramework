e2a9ab82599dbbfa922d0b945d8d854df389c3ab
zp <859247268@qq.com>
2020-09-23 15:39:04 +0800

    全局异常处理中间件

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const Koa = require('koa')
 const InitManager = require("./core/init")
+const catchError = require("./middlewares/exception")
 
 const app = new Koa()
 
 InitManager.initCore(app)
+
+// 注册错误处理中间件：
+app.use(catchError)
 
 app.listen(3000, () => {
     console.log("程序已运行在3000端口...")
