77e3d044bab004fddc16b41e44802ef532d71ab9
zp <859247268@qq.com>
2020-09-23 13:20:08 +0800

    requireDirectory实现路由自动导入

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const Koa = require('koa')
+const Router = require("koa-router")
 const book = require("./api/v1/book")
 const classic = require("./api/v1/classic")
+const requireDirectory = require("require-directory")
 
 const app = new Koa()
+
+requireDirectory(module, './api', {
+    visit: whenLoadModule
+})
+
+function whenLoadModule(obj) {
+    if (obj instanceof Router) {
+        app.use(obj.routes())
+    }
+}
 
 app.use(book.routes())
 app.use(classic.routes())
