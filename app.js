03478b1c461d24368b0fa8d97f244b7c3ae51944
zp <859247268@qq.com>
2020-09-23 12:25:54 +0800

    多router拆分

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const Koa = require('koa')
+const book = require("./api/v1/book")
+const classic = require("./api/v1/classic")
+
 const app = new Koa()
+
+app.use(book.routes())
+app.use(classic.routes())
 
 app.listen(3000, () => {
     console.log("程序已运行在3000端口...")
