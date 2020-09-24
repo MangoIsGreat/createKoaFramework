e322e62e38856e1a934e259f7b230b6e951b8ea2
zp <859247268@qq.com>
2020-09-24 17:54:15 +0800

    模型的set操作

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -1,16 +1,13 @@
 const Router = require("koa-router")
-const bcrypt = require("bcryptjs")
 const { User } = require("../../models/user")
 const router = new Router({ prefix: "/v1/user" })
 const { RegisterValidator } = require("../../validators/validator")
 
 router.post("/register", async(ctx) => {
     const v = await new RegisterValidator().validate(ctx)
-    const salt = bcrypt.genSaltSync(10)
-    const pwd = bcrypt.hashSync(v.get("body.password2", salt))
     const user = {
         nickname: v.get("body.nickname"),
-        email: pwd,
+        email: v.get("body.email"),
         password: v.get("body.password2")
     }
     const r = await User.create(user)
--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,5 @@
 const { db } = require("../../core/db")
+const bcrypt = require("bcryptjs")
 const { Sequelize, Model } = require("sequelize")
 
 class User extends Model {
@@ -12,7 +13,15 @@ User.init({
         autoIncrement: true
     },
     nickname: Sequelize.STRING,
-    password: Sequelize.STRING,
+    password: {
+        type: Sequelize.STRING,
+        // 这里采用的是 观察者模式：
+        set(val) {
+            const salt = bcrypt.genSaltSync(10)
+            const pwd = bcrypt.hashSync(val, salt)
+            this.setDataValue("password", pwd)
+        }
+    },
     email: {
         type: Sequelize.STRING(128),
         unique: true
