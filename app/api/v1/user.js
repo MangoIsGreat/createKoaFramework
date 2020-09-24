752c83b32c18de7848ff243c11041c48bf4de768
zp <859247268@qq.com>
2020-09-24 18:18:20 +0800

    success操作成功处理

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -2,6 +2,7 @@ const Router = require("koa-router")
 const { User } = require("../../models/user")
 const router = new Router({ prefix: "/v1/user" })
 const { RegisterValidator } = require("../../validators/validator")
+const { success } = require("../../lib/helper")
 
 router.post("/register", async(ctx) => {
     const v = await new RegisterValidator().validate(ctx)
@@ -11,6 +12,7 @@ router.post("/register", async(ctx) => {
         password: v.get("body.password2")
     }
     const r = await User.create(user)
+    success()
 })
 
 module.exports = router
--- core/http-exception.js
+++ core/http-exception.js
@@ -16,7 +16,17 @@ class ParameterException extends HttpException {
     }
 }
 
+class Success extends HttpException {
+    constructor(msg, errorCode) {
+        super()
+        this.msg = msg || 'ok'
+        this.errorCode = errorCode || 0
+        this.code = 201
+    }
+}
+
 module.exports = {
     HttpException,
-    ParameterException
+    ParameterException,
+    Success
 }
