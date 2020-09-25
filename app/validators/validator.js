c799227b4b38372b2f31585cded7481b8986f149
zp <859247268@qq.com>
2020-09-25 17:04:12 +0800

    模拟枚举

--- app/validators/validator.js
+++ app/validators/validator.js
@@ -1,5 +1,6 @@
-const { LinValidator, Rule } = require("../../core/lin-validator")
+const { LinValidator, Rule } = require("../../core/lin-validator-v2")
 const { User } = require("../models/user")
+const { loginType } = require("../lib/enum")
 
 class PositiveIntegerValidator extends LinValidator {
     constructor() {
@@ -54,7 +55,37 @@ class RegisterValidator extends LinValidator {
     }
 }
 
+class TokenValidator extends LinValidator {
+    constructor() {
+        super()
+        this.account = [
+            new Rule('isLength', '不符合账号规则', {
+                min: 4,
+                max: 32
+            })
+        ]
+        this.secret = [
+            new Rule('isOptional'),
+            new Rule('isLength', '至少6个字符', {
+                min: 6,
+                max: 128
+            })
+        ]
+    }
+
+    validateLoginType(vals) {
+        if (!vals.body.type) {
+            throw new Error("type是必须参数")
+        }
+
+        if (!loginType.isThisType(vals.body.type)) {
+            throw new Error("type参数不合法")
+        }
+    }
+}
+
 module.exports = {
     PositiveIntegerValidator,
-    RegisterValidator
+    RegisterValidator,
+    TokenValidator
 }
