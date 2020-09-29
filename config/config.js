14fca73150e6ab45374cab456c4f53101c8b63e2
zp <859247268@qq.com>
2020-09-29 23:30:13 +0800

    设计获取详情数据接口

--- config/config.js
+++ config/config.js
@@ -16,5 +16,9 @@ module.exports = {
         appId: 'wx859326cab90073dc',
         appSecret: '7a45f869434888417a4010161af42555',
         loginUrl: 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
-    }
+    },
+    yushu: {
+        detailUrl: 'http://t.yushu.im/v2/book/id/%s',
+        keywordUrl: 'http://t.yushu.im/v2/book/search?q=%s&count=%s&start=%s&summary=%s'
+    },
 }
