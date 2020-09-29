066c0aa64c3d0a871aaa9fa3b1a94ab245d67b8e
zp <859247268@qq.com>
2020-09-29 22:30:57 +0800

    获取热门图书列表接口改造，合并数据

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -3,9 +3,9 @@ const router = new Router({ prefix: '/v1/book' })
 const { HotBook } = require('../../models/hot-book')
 
 router.get("/hot_list", async(ctx, next) => {
-    const favors = await HotBook.getAll()
+    const books = await HotBook.getAll()
     ctx.body = {
-        books: favors
+        books
     }
 })
 
